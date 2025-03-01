b0868051f7c26e7b88b57ba4815b66811c673a42
Sabari <sabarisanthosh85@gmail.com>
2025-03-01 20:41:27 +0530

    Authentication Added

--- js/SignUp_LogIn_Form.js
+++ js/SignUp_LogIn_Form.js
@@ -1,11 +1,20 @@
+// SignUp_LogIn_Form.js
+import { signInWithGoogle } from "../firebase.js";
+
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
-})
+});
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
-})
+});
+
+const googleSignInBtn = document.getElementById("google-signin-btn");
+googleSignInBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  signInWithGoogle();
+});
