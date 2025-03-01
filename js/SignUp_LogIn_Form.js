c3d35b5367b8298d88bd9777f5019b9d4647ebdb
Sabari <sabarisanthosh85@gmail.com>
2025-03-01 22:30:32 +0530

    Authetication configured

--- js/SignUp_LogIn_Form.js
+++ js/SignUp_LogIn_Form.js
@@ -1,5 +1,6 @@
 // SignUp_LogIn_Form.js
-import { signInWithGoogle } from "../firebase.js";
+import { signInWithGoogle,signUpWithEmailPassword,
+  loginWithEmailPassword } from "../firebase.js";
 
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
@@ -17,4 +18,22 @@ const googleSignInBtn = document.getElementById("google-signin-btn");
 googleSignInBtn.addEventListener("click", (event) => {
   event.preventDefault();
   signInWithGoogle();
+});
+
+// Email/Password Registration
+const registerForm = document.getElementById("register-form");
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = registerForm.querySelector('input[type="email"]').value;
+  const password = registerForm.querySelector('input[type="password"]').value;
+  signUpWithEmailPassword(email, password);
+});
+
+// Email/Password Login
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = loginForm.querySelector('input[type="text"]').value;
+  const password = loginForm.querySelector('input[type="password"]').value;
+  loginWithEmailPassword(email, password);
 });
